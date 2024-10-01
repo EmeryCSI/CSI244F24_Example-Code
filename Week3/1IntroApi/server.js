10ee3e8f13c72ac6a9b3045bc46608a49ad61e17
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-01 10:18:28 -0700

    Updated

--- Week3/1IntroApi/server.js
+++ Week3/1IntroApi/server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 //Define Middleware
 //middleware is simply functions that run on every request
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //sample data
@@ -26,6 +27,32 @@ let courses = [
 //A GET request should NOT change or create any data
 app.get("/", (req, res) => {
   res.send("Working");
+});
+
+//GET All Courses
+app.get("/courses", (req, res) => {
+  res.json(courses);
+});
+//GET just one course by id
+//route parameter :param
+//if a parameter is passed it will be available in req.params
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const course = courses.find((c) => c.id === parseInt(id));
+  //how do we check to see if we found anything
+  //truthy falsy values
+  if (!course) {
+    return res.status(404).send("Course not found");
+  }
+  //we know we found a course
+  res.json(course);
+});
+
+//POST request typically used to create a new record
+//POST request passes data in the body
+app.post("/courses", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(PORT, () => {
