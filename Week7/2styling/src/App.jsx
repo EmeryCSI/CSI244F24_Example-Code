5301a1d4386ca61011755ca428716fd1d3b3e3ab
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-31 09:53:46 -0700

    example code updated

---
+++ Week7/2styling/src/App.jsx
@@ -0,0 +1,12 @@
+import MyCard from "./Components/MyCard";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <MyCard title={"Card Title"} description="Card Description" />
+    </div>
+  );
+}
+
+export default App;
