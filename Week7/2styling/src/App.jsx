f01239f06bdf0ae5bb9691cf4106fa5011364976
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2024-10-31 10:20:45 -0700

    example code updated

--- Week7/2styling/src/App.jsx
+++ Week7/2styling/src/App.jsx
@@ -1,10 +1,19 @@
 import MyCard from "./Components/MyCard";
+import CardStyledInComponent from "./Components/CardStyledInComponent";
+import CardStyledWithProps from "./Components/CardStyledWithProps";
 import "./App.css";
 function App() {
   return (
     <div>
       <h1>App</h1>
       <MyCard title={"Card Title"} description="Card Description" />
+      <CardStyledInComponent />
+      <CardStyledWithProps
+        backgroundColor="darkgreen"
+        titleColor="lightseagreen"
+        descriptionColor="mintcream"
+        buttonColor="forestgreen"
+      />
     </div>
   );
 }
